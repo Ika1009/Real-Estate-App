cd28a94f469f41de0cc3ed63176accbd6b8da5a5
joemama <nikola.andjelkovic.nix@gmail.com>
2023-05-19 02:54:41 +0200

    2. strana

---
+++ NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Real_Estate_App;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NewPage2.xaml.cs
@@ -0,0 +1,9 @@
+namespace Real_Estate_App;
+
+public partial class NewPage2 : ContentPage
+{
+	public NewPage2()
+	{
+		InitializeComponent();
+	}
+}
