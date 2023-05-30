6d5410b37d1df7036f6ca9f739afaf196a4641c3
Ika1009 <ilija.doncic@yahoo.com>
2023-05-30 23:39:19 +0200

    Added color change to red on the function call

--- CreateAccountPage.xaml.cs
+++ CreateAccountPage.xaml.cs
@@ -16,4 +16,9 @@ public partial class NewPage3 : ContentPage
     {
         await Navigation.PopAsync(); // I have used Navigation Stack, but this can be changed depending on what you want
     }
+    // red - RGB (249, 90, 90)
+    private void ChangeToRedBorder1() { border1.Stroke = Color.FromRgb(249, 90, 90); }
+    private void ChangeToRedBorder2() { border2.Stroke = Color.FromRgb(249, 90, 90); }
+    private void ChangeToNormalBorder1() { border1.Stroke = Colors.Transparent; }
+    private void ChangeToNormalBorder2() { border2.Stroke = Colors.Transparent; }
 }
--- SignInPage.xaml.cs
+++ SignInPage.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace Real_Estate_App;
 
 public partial class NewPage4 : ContentPage
@@ -5,10 +6,16 @@ public partial class NewPage4 : ContentPage
 	public NewPage4()
 	{
 		InitializeComponent();
-	}
+    }
 
     private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
         await Navigation.PushAsync(new NewPage3()); // I have used Navigation Stack, but this can be changed depending on what you want
     }
+
+    // red - RGB (249, 90, 90)
+    private void ChangeToRedBorder1() { border1.Stroke = Color.FromRgb(249, 90, 90); }
+    private void ChangeToRedBorder2() { border2.Stroke = Color.FromRgb(249, 90, 90); }
+    private void ChangeToNormalBorder1() { border1.Stroke = Colors.Transparent; }
+    private void ChangeToNormalBorder2() { border2.Stroke = Colors.Transparent; }
 }
